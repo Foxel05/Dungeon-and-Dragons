cfa90ad872e62ff4585d3eb1b1736b36f978f436
Foxel05 <56955651+Foxel05@users.noreply.github.com>
2020-12-20 20:05:38 +0100

    Added all Klassen and a not fully working proof system

--- assets/Charakter_Creation/Creation_1.js
+++ assets/Charakter_Creation/Creation_1.js
@@ -1,22 +1,71 @@
-var Klasse = ["-", "Barbar", "Barde", "Druide", "Hexenmeister", "Kleriker", "Kämpfer", "Magier", "Mönch", "Paladin", "Schurke", "Sidekick: Krieger", "Sidekick: Spezalist", "Sidekick: Zauberwirker", "Waldläufer", "Zauberer"];
-var Volk = ["-", "Drachenblütiger", "Elf", "Firbolg", "Gnom", "Grottenschrat", "Halb-Elf", "Halb-Ork", "Halbling", "Mensch", "Minitaurus", "Tiefling", "Zwerg"];
-var Unterart = ["-", "Devkarin", "Drow", "Gebirgszwerg", "Grün", "Hochelf", "Hügelzwerg", "Leichfuß", "Rot", "Stämmiger", "Variante", "Waldelf", "Waldgnom", "Weiß", "Wilder"];
-var Hintergrund = ["-", "Adliger", "Boros Legion", "Einsiedler", "Geplagter", "Gildenhandwerker", "Golgari Swarm", "Krimineller", "Rakdos Cultist", "Scharlatan", "Soldat", "Sonderling", "Spion", "Straßenkind", "Stadtischer Kopfgeldjäger", "Söldnerveteran", "Tempeldiener", "Unterhaltungskünstler", "Volksheld", "Wanderer aus der Ferne", "Weiser"];
+var Klasse = ["-", "Barbar", "Barde", "Druide", "Hexenmeister", "Kleriker", "Kämpfer", "Magier", "Mönch", "Paladin", "Schurke", "Waldläufer", "Zauberer"];
+var Volk = ["-", "Drachenblütiger", "Elf", "Gnom", "Halb-Elf", "Halb-Ork", "Halbling", "Mensch", "Tiefling", "Zwerg"];
+var Unterart = ["-", "Drow", "Grün", "Hochelf", "Hügelzwerg", "Stämmiger", "Variante", "Waldelf", "Waldgnom"];
+var Hintergrund = ["-", "Adliger", "Einsiedler", "Gildenhandwerker", "Krimineller", "Scharlatan", "Soldat", "Sonderling", "Straßenkind", "Tempeldiener", "Unterhaltungskünstler", "Volksheld", "Weiser"];
 
 //Klassen List
 //Barbar
-var Barbar_Volk = ["-", "Drachenblütiger", "Halb-Ork"];
-var Barbar_Unterart = ["-", "Rot"];
-var Barbar_Hintergrund = ["-", "Gildenhandwerker", "Sonderling"];
+var Barbar_Volk = ["-",  "Halb-Ork"];
+var Barbar_Unterart = ["-"];
+var Barbar_Hintergrund = ["-", "Sonderling"];
 //Barde
-var Barde_Volk = ["-", "Elf", "Zwerg"];
-var Barde_Unterart = ["-", "Drow", "Hügelzwerg"];
-var Barde_Hintergrund = ["-"];
+var Barde_Volk = ["-", "Elf"];
+var Barde_Unterart = ["-", "Drow"];
+var Barde_Hintergrund = ["-", "Unterhaltungskünstler"];
+//Druide
+var Druide_Volk = ["-", "Elf"];
+var Druide_Unterart = ["-", "Waldelf"];
+var Druide_Hintergrund = ["-", "Gildenhandwerker"];
+//Hexenmeister
+var Hexenmeister_Volk = ["-", "Tiefling"];
+var Hexenmeister_Unterart = ["-"];
+var Hexenmeister_Hintergrund = ["-", "Scharlatan"];
+//Kleriker
+var Kleriker_Volk = ["-", "Zwerg"];
+var Kleriker_Unterart = ["-", "Hügelzwerg"];
+var Kleriker_Hintergrund = ["-", "Templeldiener"];
+//Kämpfer
+var Kämpfer_Volk = ["-", "Drachenblütiger"];
+var Kämpfer_Unterart = ["-", "Grün"];
+var Kämpfer_Hintergrund = ["-", "Soldat"];
+//Magier
+var Magier_Volk = ["-", "Elf"];
+var Magier_Unterart = ["-", "Hochelf"];
+var Magier_Hintergrund = ["-", "Weiser"];
+//Mönch
+var Mönch_Volk = ["-", "Halbling"];
+var Mönch_Unterart = ["-", "Stämmiger"];
+var Mönch_Hintergrund = ["-", "Straßenkind"];
+//Paladin
+var Paladin_Volk = ["-", "Mensch"];
+var Paladin_Unterart = ["-", "Variante"];
+var Paladin_Hintergrund = ["-", "Adeliger"];
+//Schurke
+var Schurke_Volk = ["-", "Gnom"];
+var Schurke_Unterart = ["-", "Waldgnom"];
+var Schurke_Hintergrund = ["-", "Krimineller"];
+//Waldläufer
+var Waldläufer_Volk = ["-", "Elf"];
+var Waldläufer_Unterart = ["-" ,"Waldelf"];
+var Waldläufer_Hintergrund = ["-", "Volksheld"];
+//Zauberer
+var Zauberer_Volk = ["-", "Halb-Elf"];
+var Zauberer_Unterart = ["-"];
+var Zauberer_Hintergrund = ["-", "Einsiedler"];
 
 //Volk List
-var Drachenblütiger_Klasse = ["-", "Barbar", "Kleriker", "Kämpfer", "Waldläufer"];
-var Drachenblütiger_Unterart = ["-", "Grün", "Rot", "Weiß"];
-var Drachenblütiger_Hintergrund = ["-", "Einsiedler", "Gildenhandwerker", "Soldat"];
+//Drachenblütiger
+var Drachenblütiger_Klasse = ["-", "Kämpfer"];
+var Drachenblütiger_Unterart = ["-", "Grün"];
+var Drachenblütiger_Hintergrund = ["-", "Soldat"];
+//Elf
+var Elf_Klasse = ["-", "Barde", "Druide", "Magier", "Waldläufer"];
+var Elf_Unterart = ["-", "Drow", "Waldelf", "Hochelf"];
+var Elf_Hintergrund = ["-", "Unterhaltungskünstler", "Gildenhandwerker", "Weiser", "Volksheld"];
+//Gnom
+var Gnom_Klasse = ["-", "Schurke"];
+var Gnom_Unterart = ["-", "Waldgnom"];
+var Gnom_Hintergrund = ["-", "Krimineller"];
 
 // Nothing Selected          
 
@@ -94,6 +143,67 @@ function Selected(a){
 				ObjectVar_Klasse_Unterart = Barde_Unterart;
 				ObjectVar_Klasse_Hintergrund = Barde_Hintergrund;
 			}
+			//Druide
+			else if (SelectedObject == "Druide"){
+				ObjectVar_Klasse_Volk = Druide_Volk;
+				ObjectVar_Klasse_Unterart = Druide_Unterart;
+				ObjectVar_Klasse_Hintergrund = Druide_Hintergrund;
+			}
+			//Hexenmeister
+			else if (SelectedObject == "Hexenmeister"){
+				ObjectVar_Klasse_Volk = Hexenmeister_Volk;
+				ObjectVar_Klasse_Unterart = Hexenmeister_Unterart;
+				ObjectVar_Klasse_Hintergrund = Hexenmeister_Hintergrund;
+			}
+			//Kleriker
+			else if (SelectedObject == "Kleriker"){
+				ObjectVar_Klasse_Volk = Kleriker_Volk;
+				ObjectVar_Klasse_Unterart = Kleriker_Unterart;
+				ObjectVar_Klasse_Hintergrund = Kleriker_Hintergrund;
+			}
+			//Magier
+			else if (SelectedObject == "Magier"){
+				ObjectVar_Klasse_Volk = Magier_Volk;
+				ObjectVar_Klasse_Unterart = Magier_Unterart;
+				ObjectVar_Klasse_Hintergrund = Magier_Hintergrund;
+			}
+			//Kämpfer
+			else if (SelectedObject == "Kämpfer"){
+				ObjectVar_Klasse_Volk = Kämpfer_Volk;
+				ObjectVar_Klasse_Unterart = Kämpfer_Unterart;
+				ObjectVar_Klasse_Hintergrund = Kämpfer_Hintergrund;
+			}
+			//Mönch
+			else if (SelectedObject == "Mönch"){
+				ObjectVar_Klasse_Volk = Mönch_Volk;
+				ObjectVar_Klasse_Unterart = Mönch_Unterart;
+				ObjectVar_Klasse_Hintergrund = Mönch_Hintergrund;
+			}
+			//Paladin
+			else if (SelectedObject == "Paladin"){
+				ObjectVar_Klasse_Volk = Paladin_Volk;
+				ObjectVar_Klasse_Unterart = Paladin_Unterart;
+				ObjectVar_Klasse_Hintergrund = Paladin_Hintergrund;
+			}
+			//Schurke
+			else if (SelectedObject == "Schurke"){
+				ObjectVar_Klasse_Volk = Schurke_Volk;
+				ObjectVar_Klasse_Unterart = Schurke_Unterart;
+				ObjectVar_Klasse_Hintergrund = Schurke_Hintergrund;
+			}
+			//Waldläufer
+			else if (SelectedObject == "Waldläufer"){
+				ObjectVar_Klasse_Volk = Waldläufer_Volk;
+				ObjectVar_Klasse_Unterart = Waldläufer_Unterart;
+				ObjectVar_Klasse_Hintergrund = Waldläufer_Hintergrund;
+			}
+			//Zauberer
+			else if (SelectedObject == "Zauberer"){
+				ObjectVar_Klasse_Volk = Zauberer_Volk;
+				ObjectVar_Klasse_Unterart = Zauberer_Unterart;
+				ObjectVar_Klasse_Hintergrund = Zauberer_Hintergrund;
+			}
+
 			//Bearbeitet Volk nach Auswahl
 			var string="";
 			for(i=0;i<ObjectVar_Klasse_Volk.length;i++){
@@ -123,23 +233,52 @@ function Selected(a){
 			ObjectVar_Volk_Unterart = Drachenblütiger_Unterart;
 			ObjectVar_Volk_Hintergrund = Drachenblütiger_Hintergrund;
 		}
+		//Elf
+		else if (SelectedObject == "Elf"){
+			ObjectVar_Volk_Klasse = Elf_Klasse;
+			ObjectVar_Volk_Unterart = Elf_Unterart;
+			ObjectVar_Volk_Hintergrund = Elf_Hintergrund;
+		}
+		//Gnom
+		else if (SelectedObject == "Gnom"){
+			ObjectVar_Volk_Klasse = Gnom_Klasse;
+			ObjectVar_Volk_Unterart = Gnom_Unterart;
+			ObjectVar_Volk_Hintergrund = Gnom_Hintergrund;
+		}
+		//Überprüfet die Auswahl bei Klasse
 		for(i=0;i<ObjectVar_Volk_Klasse.length;i++){
 			if (ObjectVar_Volk_Klasse[i]==ActiveKlasse){
-				var string="";
-				for(let i=0;i<ObjectVar_Volk_Klasse.length;i++){
-					if (ActiveKlasse.match(ObjectVar_Volk_Klasse[i])){
-						for(let j=0;j<ObjectVar_Klasse_Hintergrund.length;j++){
-							for(let i=0;i<ObjectVar_Volk_Hintergrund.length;i++){
-								if (ObjectVar_Klasse_Hintergrund[j].match(ObjectVar_Volk_Hintergrund[i])){
-									var res = ObjectVar_Klasse_Hintergrund[j].match(ObjectVar_Volk_Hintergrund[i]);
-									string=string+"<option value="+res+">"+res+"</option>";
-									document.getElementById("Hintergrund").innerHTML=string;
-									alert(res);
-								}
-							}
+				//Hintergrund
+				//Guckt sich beide Listen an (Was steht bei Hintergrund zu Auswahl)
+				for(let j=0;j<ObjectVar_Klasse_Hintergrund.length;j++){
+					for(let i=0;i<ObjectVar_Volk_Hintergrund.length;i++){
+						//Vergleicht beide Listen und übernimmt nur das was in beiden vorkommt
+						if (ObjectVar_Klasse_Hintergrund[j].match(ObjectVar_Volk_Hintergrund[i])){
+							//Fügt die übernommenden Listenelement in die Html zur Auswahl ein
+							var res_Hin = ObjectVar_Klasse_Hintergrund[j].match(ObjectVar_Volk_Hintergrund[i]);
+							string=string+"<option value="+res_Hin+">"+res_Hin+"</option>";
+							document.getElementById("Hintergrund").innerHTML=string;
+							alert("Ähnlichkeiten in Hintergrund: "+res_Hin);
 						}
 					}
 				}
+				//Unterart
+				//Guckt sich beide Listen an (Was steht bei Unterart zu Auswahl)
+				for(let j=0;j<ObjectVar_Klasse_Unterart.length;j++){
+					for(let i=0;i<ObjectVar_Volk_Unterart.length;i++){
+						//Vergleicht beide Listen und übernimmt nur das was in beiden vorkommt
+						if (ObjectVar_Klasse_Unterart[j].match(ObjectVar_Volk_Unterart[i])){
+							//Fügt die übernommenden Listenelement in die Html zur Auswahl ein
+							var res_Unt = ObjectVar_Klasse_Unterart[j].match(ObjectVar_Volk_Unterart[i]);
+							string=string+"<option value="+res_Unt+">"+res_Unt+"</option>";
+							document.getElementById("Unterart").innerHTML=string;
+							alert("Ähnlichkeiten in Unterart: "+res_Unt);
+						}
+					}
+				}
+			}
+			else {
+				alert("Hilfe!");
 			}
 		}
 	}
